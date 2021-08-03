d643f4d04f3a310da61c0f8179d42c8113bb7626
rkique <eriq.xia@gmail.com>
2021-08-03 13:01:34 -0700

    md

---
+++ public/blogroll.js
@@ -0,0 +1,12 @@
+links = [
+    "https://www.nintil.com",
+    "https://www.gwern.net",
+    "https://applieddivinitystudies.com/",
+    "https://noahpinion.substack.com/",
+    "https://whitehotharlots.tumblr.com/",
+    "https://www.piratewires.com/",
+    "https://kneelingbus.substack.com/",
+    "https://www.xkcd.com"
+   ]
+
+document.getElementById("random_blog").href = links[Math.floor(Math.random()*links.length)];
