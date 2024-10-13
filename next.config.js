1fa499854a179ab3ffeabce0e9c8eaeef0d45e7a
Eric Xia <ericpingxia@gmail.com>
2024-10-13 12:29:22 -0400

    %*

--- next.config.js
+++ next.config.js
@@ -1,27 +1,14 @@
-const path = require('path');
-
-module.exports = {
-  output: {
-    filename: 'my-first-webpack.bundle.js',
-  },
-  module: {
-    loaders: [{
-      test: /.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.css$/,
-      loader: "style-loader!css-loader"
-    }, {
-      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-      loader: 'url-loader?limit=100000' }]
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: "/dir/:customPage",
-        destination: "/dir/:customPage/index.html",
-      }
-    ]
-},
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
